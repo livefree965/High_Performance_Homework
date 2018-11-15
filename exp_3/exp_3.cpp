517e4b816122f0e123fcb6e70fa6b5b6c92ea72c
xiejiangzhao <xiejzh3@mail2.sysu.edu.cn>
2018-11-15 16:18:16 +0800

    first finish version

--- exp_3/exp_3.cpp
+++ exp_3/exp_3.cpp
@@ -16,6 +16,7 @@
 #include <semaphore.h>
 #include <iomanip>
 #include <memory.h>
+#include <math.h>
 
 using namespace std;
 int Mat_size;
@@ -78,13 +79,13 @@ int **read_mat(int beg_row, int beg_col, int part_size, string filename) {
     ifstream infile;
     infile.open(filename);
     int tmp[3]; //用来存储解析到的三元组数据
-    char data[Mat_size]; //用来存储读取的每一行的数据
+    char data[20]; //用来存储读取的每一行的数据
     int **res = new int *[Mat_size]; //用来保存矩阵数据，是一个二元数组
     for (int i = 0; i < Mat_size; ++i) {
         res[i] = new int[Mat_size];
     }
     for (int i = 0; i < part_size; ++i) {
-        infile.seekg(((beg_row + i) * Mat_size + beg_col) * Mat_size); 
+        infile.seekg(((beg_row + i) * Mat_size + beg_col) * 16);
         //定位到矩阵元素所在的位置
         for (int j = 0; j < part_size; ++j) {
             //超出边界则设定值为0
@@ -92,7 +93,7 @@ int **read_mat(int beg_row, int beg_col, int part_size, string filename) {
                 res[i][j] = 0;
                 continue;
             }
-            infile.getline(data, Mat_size); //读取一行数据
+            infile.getline(data, 20); //读取一行数据
             get_element(data, tmp); //解析数据并保存到tmp中
             res[i][j] = tmp[2]; //将数据放入二元数组
         }
@@ -110,15 +111,15 @@ int *read_vec(int *res, int beg_row, int part_size, string filename) {
     ifstream infile;
     infile.open(filename);
     int tmp[3];
-    char data[Mat_size];
+    char data[20];
     for (int i = 0; i < part_size; ++i) {
         if (beg_row + i >= Mat_size) {
-        //如果读取超出边界则设为0
+            //如果读取超出边界则设为0
             res[i] = 0;
             continue;
         }
         infile.seekg((beg_row + i) * Row_char_count);
-        infile.getline(data, Mat_size);
+        infile.getline(data, 20);
         get_element(data, tmp); //解析当行数据
         res[i] = tmp[2]; //存入一维数组
     }
@@ -126,7 +127,10 @@ int *read_vec(int *res, int beg_row, int part_size, string filename) {
 }
 
 int main(int argc, char *argv[]) {
-    Mat_size = 16;
+    if (argc == 2) {
+        Mat_size = int(pow(2, atoi(argv[1])));
+    } else
+        Mat_size = 64;
 //    int **mat1_data = read_mat(0, 0, Mat_size, "mat.data");
 //    int *vec1_data = new int[Mat_size];
 //    read_vec(vec1_data, 0, Mat_size, "vec.data");
@@ -144,14 +148,12 @@ int main(int argc, char *argv[]) {
 //    int *tmp = read_vec(1, 2, "vec.data");
 //    int **all = read_mat(0, 0, Mat_size, "mat.data");
 //    show_mat(all, 16, "all.data");
-//    generate_mat(Mat_size, Mat_size, "mat.data");
-//    generate_mat(Mat_size, 1, "vec.data");
 //    show_mat(res, 3);
     int my_rank, comm_size;
     int rankX, rankY;
     int ndims = 2; //维数
     int periods[2] = {0, 0}; //关乎进程的位置关系，我们默认第一个和最后一个不连接
-    int reorder = 0; 
+    int reorder = 0;
     int remainX[2] = {1, 0}; //用于创建子通信域
     int remainY[2] = {0, 1}; //用于创建子通信域
     MPI_Comm comm2d; //笛卡尔拓扑所有进程的通信域
@@ -159,6 +161,11 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+    if (my_rank == 0) {
+        srand(3);
+        generate_mat(Mat_size, Mat_size, "mat.data");
+        generate_mat(Mat_size, 1, "vec.data");
+    }
     int dims[2] = {int(sqrt(comm_size)), int(sqrt(comm_size))}; //我们拓扑成正方形的
     int process_dim = dims[0]; //拓扑的一行/列进程数
     MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &comm2d); //创建拓扑
@@ -166,10 +173,8 @@ int main(int argc, char *argv[]) {
     MPI_Cart_sub(comm2d, remainY, &commY); //创建行的子通信域
     MPI_Comm_rank(commX, &rankX); //获取进程在当列的下标
     MPI_Comm_rank(commY, &rankY); //获取进程在当行的下标
-    int process_mat_size = 1;
-    while (process_mat_size * sqrt(comm_size) < Mat_size) {
-        process_mat_size++;
-    } //确定每个进程分配到的矩阵的大小(列数/行数)，超出矩阵部分会被赋为0
+    double begin = MPI_Wtime();
+    int process_mat_size = int(ceil(Mat_size / sqrt(comm_size)));
 //    Mat_size = process_mat_size * int(sqrt(comm_size));
 //    int process_mat_size = int(Mat_size / sqrt(comm_size));
     int **mat_data = read_mat(rankX * process_mat_size, rankY * process_mat_size, process_mat_size, "mat.data");
@@ -192,7 +197,9 @@ int main(int argc, char *argv[]) {
     MPI_Reduce(res, res_sum, process_mat_size, MPI_INT, MPI_SUM, 0, commY);
     MPI_Gather(res_sum, process_mat_size, MPI_INT, all_sum, process_mat_size, MPI_INT, 0, commX);
     if (my_rank == 0) {
-        show_vec(all_sum, Mat_size, "sum.mtx");
+        double end = MPI_Wtime() - begin;
+        cout << "Mat size: " << Mat_size << " Cores:" << comm_size << " Use time: " << end << endl;
+//        show_vec(all_sum, Mat_size, "sum.mtx");
     }
 //    printf("rank = %d;   X = %d;  Y = %d\n", my_rank, rankX, rankY);
 //    MPI_Comm top_comm;
